d5430d3bea21a4e2de1b20d1befadd84b6167dd5
nioclandramer <161773056+nioclandramer@users.noreply.github.com>
2024-11-08 17:51:38 -0500

    Entidades

--- src/main/java/com/IntWeb/Entidades/Usuario.java
+++ src/main/java/com/IntWeb/Entidades/Usuario.java
@@ -1,10 +1,17 @@
 package com.IntWeb.Entidades;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 public abstract class Usuario {
     @Id
